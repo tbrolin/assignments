4d5b82fc88cb0ebb8e95896e5ccc32820931773a
Tobbe Brolin <tobias.brolin@atex.com>
2018-12-19 00:38:20 +0100

    Added comments.

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// http service listening on port 8181
+// for posts on /hello and respond with
+// a simple message
+
 const http = require ('http');
 const url = require ('url');
 
 const server = http.createServer ((request, response) => {
-  let requestUrl = url.parse (request.url, true);
+
+  // parse the url from request
+  let requestUrl = url.parse (request.url);
+
+  // remove any jamming slashes from pathname
   let path = requestUrl.pathname.replace (/^\/+|\/+$/g, '');
+
+  // check method and path and respond
   if (request.method.toUpperCase () === 'POST' && path === 'hello') {
     response.setHeader ('Content-Type', 'application/json');
     response.end ('{"Welcome":"How are you?"}');
