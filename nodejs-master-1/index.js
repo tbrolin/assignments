2fc1c08ea3edc7b6b60a5760b3dabbabf4090441
Tobbe <tobias.brolin@atex.com>
2020-02-17 10:33:40 +0100

    Move files. GET instead of POST

--- nodejs-master-1/index.js
+++ nodejs-master-1/index.js
@@ -1,12 +1,11 @@
 // http service listening on port 8181
-// for posts on /hello and respond with
+// for gets on /hello and respond with
 // a simple message
 
 const http = require ('http');
 const url = require ('url');
 
 const server = http.createServer ((request, response) => {
-
   // parse the url from request
   let requestUrl = url.parse (request.url);
 
@@ -14,8 +13,8 @@ const server = http.createServer ((request, response) => {
   let path = requestUrl.pathname.replace (/^\/+|\/+$/g, '');
 
   // check method and path and respond
-  if (request.method.toUpperCase () === 'POST' && path === 'hello') {
-    response.setHeader ('Content-Type', 'application/json');
+  if (request.method.toUpperCase () === 'GET' && path === 'hello') {
+    response.setHeader ('content-type', 'application/json');
     response.end ('{"Welcome":"How are you?"}');
   } else {
     response.writeHead (404);
